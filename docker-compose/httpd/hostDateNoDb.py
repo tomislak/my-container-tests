dd2a3e27c5c2f83e40a0f309ca8faba295d6ba86
Tomislav Kralj <tomislak@gmail.com>
2021-08-31 11:32:03 +0200

    image creation for httpd and compose for nginx and httpd

---
+++ docker-compose/httpd/hostDateNoDb.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+import platform
+from datetime import datetime
+
+if __name__ == '__main__':
+
+    print("Content-type:text/html\r\n\r\n")
+    print('<html>')
+    print('<head>')
+    print('<title>Host info</title>')
+    print('</head>')
+    print('<body>')
+    print('<h2>' + str(platform.node()) + '\t' + str(datetime.now().isoformat()) +'</h2>')
+    print('</body>')
+    print('</html>')
+
